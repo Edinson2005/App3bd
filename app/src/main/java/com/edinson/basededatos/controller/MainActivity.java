cdd81d70735c4d069f6a39b83eff0c92c60e8224
ronald13130 <ronaldpino1313@gmail.com>
2024-05-27 14:22:54 -0500

    Insertar Datos (tabla:Pais y Datos)

--- app/src/main/java/com/edinson/basededatos/controller/MainActivity.java
+++ app/src/main/java/com/edinson/basededatos/controller/MainActivity.java
@@ -7,21 +7,35 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.edinson.basededatos.model.BdHelper;
+import com.edinson.basededatos.model.ManangerBd;
 
 public class MainActivity extends AppCompatActivity {
     // DECLARA MI CLASE CONEXION
-    BdHelper bdHelper;
+    ManangerBd manangerBd;
     SQLiteDatabase bd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //HAGO UN LLAMADO A MI CONEXION
-        bdHelper = new BdHelper(MainActivity.this);
-        bd = bdHelper.getWritableDatabase();//ABRO BASE DE DATOS EN MODO ESCRITURA
 
-        Toast.makeText(this, "Base de datos creada Satisfactoriamente", Toast.LENGTH_SHORT).show();
+        manangerBd = new ManangerBd(MainActivity.this);
+        //insertar Datos Pais
 
+        long resul = manangerBd.insertDatos();
+        if(resul<0){
+            Toast.makeText(this, "No inserto" + resul, Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Datos insertados"+ resul, Toast.LENGTH_SHORT).show();
+        }
+
+        //insertar Datos DAtos
+        long result = manangerBd.insertarDatos2();
+        if(result<0){
+            Toast.makeText(this, "No inserto" + result, Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Datos insertados"+ result, Toast.LENGTH_SHORT).show();
+        }
 
     }
 
--- app/src/main/java/com/edinson/basededatos/model/ManangerBd.java
+++ app/src/main/java/com/edinson/basededatos/model/ManangerBd.java
@@ -1,4 +1,50 @@
 package com.edinson.basededatos.model;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class ManangerBd {
-}
+    BdHelper bdHelper; // Declaro la clase donde esta la conexion de la base de datos
+    SQLiteDatabase bd; //declaro variable de tipo SQlite database para los metodos de la bd
+    public ManangerBd (Context context){
+        bdHelper = new BdHelper(context); // llamo a la conexion de la base de datos
+
+    }
+    //MEtodo que abre mi base de datos en modo escritura
+    public void  openBdWr(){
+        bd= bdHelper.getWritableDatabase();
+        }
+    //MEtodo que abre mi base de datos en modo lectura
+        public void openBdRd(){
+            bd = bdHelper.getReadableDatabase();
+        }
+
+    //METODO INSERTAR DATOS
+        public long insertDatos(){
+            openBdWr();// paso 1 abro al bd en mode escritura
+            ContentValues values = new ContentValues();
+            values.put("cod","1");// paso 2 lleno el contenedor
+            values.put("nombre","Colombia");
+            long resul = bd.insert("Pais", null,values);
+
+            //en la variable resul se almacena el resultado de la iserción
+            //-1 si no inserta, o un valor mayor de cero si inserta en la bd
+            return resul;
+        }
+
+        public long insertarDatos2(){
+            openBdWr();
+            ContentValues values = new ContentValues();
+            values.put("cod","1");
+            values.put("nombre","Edinson");
+            values.put("apellido","campo");
+            values.put("edad","18");
+            values.put("correo", "example@.com");
+            values.put("telefono","3144022619");
+            long result= bd.insert("Datos",null,values);
+
+            return result;
+        }
+    }
+
